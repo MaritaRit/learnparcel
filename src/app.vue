356e3c2520fbf26f05f78ff0ce5fd25702d87d49
Marita Ritslaid <marita.ritslaid@gmail.com>
2022-05-09 15:41:25 +0300

    add addItem method

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,41 @@
 <template>
-   <div class="container">
-            <input class="input" v-model="message">
-            <h1 class="is-size-1">{{ message }}</h1>
-         </div>
+    <div class="container">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" v-model="message">
+            </div>
+            <div class="control">
+                <button class="button is-primary" v-on:click="addItem">
+                    Add Item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <ul>
+                <li v-for="(item,index) in items" v-bind:key="index">{{item}}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
+    data() {
         return {
-            message: 'Hello Vue!'
+            message: '',
+            items: ['piim', 'sai', 'leib']
+        }
+    },
+    methods: {
+        addItem(){
+            if(this.message.trim() != ''){
+                this.items.push(this.message);
+            }
+            this.message = '';
         }
     }
 }
 </script>
 
 <style>
-
 </style>
