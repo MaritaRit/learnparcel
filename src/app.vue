17d377a10123463a3ff02659969d519c2024ce3a
Marita Ritslaid <marita.ritslaid@gmail.com>
2022-05-09 18:50:27 +0300

    Add removing elements and adding on pressing enter

--- src/app.vue
+++ src/app.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="field has-addons">
             <div class="control">
-                <input class="input" v-model="message">
+                <input class="input" v-model="message" @keydown.enter="addItem">
             </div>
             <div class="control">
                 <button class="button is-primary" v-on:click="addItem">
@@ -12,7 +12,10 @@
         </div>
         <div class="content">
             <ul>
-                <li v-for="(item,index) in items" v-bind:key="index">{{item}}</li>
+                <li v-for="(item,index) in items" v-bind:key="index">
+                    {{item}}
+                    <button class="button is-primary" @click="removeItem(index)">Done</button>
+                    </li>
             </ul>
         </div>
     </div>
@@ -32,6 +35,9 @@ export default {
                 this.items.push(this.message);
             }
             this.message = '';
+        },
+        removeItem(index){
+            this.items.splice(index, 1);
         }
     }
 }
